b76f2c5d0e177176e2e2c2bcb98afb89eb94ed2c
Markos Gogoulos <mgogoulos@gmail.com>
2025-07-16 21:36:47 +0300

    all

--- {{copier__project_slug}}/backend/config/urls.py
+++ {{copier__project_slug}}/backend/config/urls.py
@@ -5,10 +5,13 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
+
 {%- if copier__create_nextjs_frontend %}
 from django.views.decorators.csrf import csrf_exempt
 from strawberry.django.views import GraphQLView
+
 from .schema import schema
+
 {%- endif %}
 
 urlpatterns = [
