a110143dd1604965c993e150419296095e49fb16
Roché Compaan <roche@sixfeetup.com>
2025-07-23 14:35:00 -0400

    feat: add nix flake (#122)

---
+++ template/flake.nix
@@ -0,0 +1,70 @@
+{
+  description = "{{ copier__project_name }}  dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (python3.withPackages (ps: with ps; [
+              black
+              botocore
+              boto3
+              bandit
+              diagrams
+              isort
+              mkdocs-material
+              mkdocs-mermaid2-plugin
+              psycopg2
+              pip
+            ]))
+            awscli2
+            argocd
+            bashInteractive
+            coreutils
+            curl
+            entr
+            envsubst
+            gnumake
+            gnused
+            go
+            go-task
+            graphviz
+            jq
+            kind
+            kubectl
+            kubernetes-helm
+            kubeseal
+            nodejs_20
+            nodePackages.eslint
+            nodePackages.prettier
+            openssh
+            opentofu
+            pass
+            pre-commit
+            podman
+            podman-compose
+            talosctl
+            tilt
+            yq
+          ];
+          shellHook = ''
+            echo "{{ copier__project_name }} dev environment shell hook"
+            export LC_ALL=en_US.UTF-8
+            export LANG=en_US.UTF-8
+            pre-commit install
+          '';
+        };
+      }
+    );
+}
