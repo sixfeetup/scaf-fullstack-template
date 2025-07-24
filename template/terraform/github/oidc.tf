4215470a71d3857abc544fe4dbbe4db6d36ea347
Roché Compaan <roche@sixfeetup.com>
2025-07-24 13:26:40 +0200

    fix(terraform): remove duplicate oidc resources

--- template/terraform/github/oidc.tf
+++ template/terraform/github/oidc.tf
@@ -79,34 +79,4 @@ resource "aws_iam_role" "github_oidc_role" {
 EOF
 }
 
-data "aws_iam_openid_connect_provider" "github" {
-  url = "https://token.actions.githubusercontent.com"
-}
 
-# Define the IAM role
-resource "aws_iam_role" "github_oidc_role" {
-  name = "{{ copier__project_slug }}-github-oidc-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Federated": "${aws_iam_openid_connect_provider.github.arn}"
-      },
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringLike": {
-          "token.actions.githubusercontent.com:sub": "repo:{{ copier__source_control_organization_slug }}/{{ copier__repo_name }}:*"
-        },
-        "StringEquals": {
-          "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
-        }
-      }
-    }
-  ]
-}
-EOF
-}
